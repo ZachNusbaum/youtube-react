a4a4dda571a67b4c596533ce40de89c3bb8b7e98
Zach Nusbaum <zach@zdnmail.com>
2019-03-11 17:10:05 -0700

    Add search form to navabr

--- src/containers/HeaderNav/HeaderNav.jsx
+++ src/containers/HeaderNav/HeaderNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Image } from 'semantic-ui-react';
+import { Menu, Image, Form, Input } from 'semantic-ui-react';
 import logo from '../../assets/images/logo.jpg';
 import './HeaderNav.scss';
 
@@ -8,6 +8,14 @@ export const HeaderNav = () => (
     <Menu.Item header className="logo">
       <Image src={logo} size="tiny" />
     </Menu.Item>
-    <Menu.Menu>{/* TODO */}</Menu.Menu>
+    <Menu.Menu className="nav-container">
+      <Menu.Item className="search-input">
+        <Form>
+          <Form.Field>
+            <Input placeholder="search" size="small" action="Go" />
+          </Form.Field>
+        </Form>
+      </Menu.Item>
+    </Menu.Menu>
   </Menu>
 );
