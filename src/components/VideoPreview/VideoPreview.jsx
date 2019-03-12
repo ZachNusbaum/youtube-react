9b745c2db49fdfed2ab189588e5c5c1b2c233cb9
Zach Nusbaum <zach@zdnmail.com>
2019-03-11 21:36:20 -0700

    Begin home feed

---
+++ src/components/VideoPreview/VideoPreview.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './VideoPreview.scss';
+
+export const VideoPreview = () => <div>This is a VideoPreview placeholder</div>;
---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { VideoPreview } from '../../components/VideoPreview/VideoPreview';
+
+export const Home = () => <VideoPreview />;
